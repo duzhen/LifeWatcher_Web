54e73a5d42e881ad3be38294b7be0072c3857c13
Ethan <haoz86@gmail.com>
2017-11-30 02:20:45 -0500

    fixed redirection

--- LifeWatcher_Web.py
+++ LifeWatcher_Web.py
@@ -293,8 +293,8 @@ def list_all_cameras(user_id):
 def hello():
     if 'credentials' not in flask.session:
         return flask.redirect('authorize')
-    return redirect('http://localhost:5000/index.html')
-    # return redirect("http://ec2-18-216-37-90.us-east-2.compute.amazonaws.com/index.html", code=302)
+    # return redirect('http://localhost:5000/index.html')
+    return redirect("http://ec2-18-216-37-90.us-east-2.compute.amazonaws.com/index.html", code=302)
 
 
 @app.route('/<path:path>')
